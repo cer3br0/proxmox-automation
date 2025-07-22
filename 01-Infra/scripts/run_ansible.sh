7e0da4a7a829750eef55f2998bea4a3d8d416cc5
cer3br0 <ludovic.lengard@email.com>
2025-07-22 16:09:56 +0200

    add vars for playbook and inventory in run_ansible.sh

--- 01-Infra/scripts/run_ansible.sh
+++ 01-Infra/scripts/run_ansible.sh
@@ -4,6 +4,8 @@ VM_IP="$2"
 TEMPLATE_NAME="$3"
 shift 3
 roles=("$@")
+INVENTORY_FILE="../02-Config/inventory.ini"
+PLAYBOOK_NAME="../02-Config/main.yaml"
 
 # Conversion des rôles en format JSON pour Ansible (plus sûr que YAML)
 roles_json=$(printf '"%s",' "${roles[@]}" | sed 's/,$//')
@@ -20,8 +22,8 @@ fi
 
 echo "Running Ansible for $VM_NAME ($VM_IP) in group $GROUP with roles: ${roles[*]} $roles_string"
 echo ""
-echo "commande lancée : ansible-playbook ../02-Config/main.yaml \
-  -i ../02-Config/inventory.ini \
+echo "commande lancée : ansible-playbook $PLAYBOOK_NAME \
+  -i $INVENTORY_FILE \
   --extra-vars "{\"target\":\"$GROUP\", \"roles\":$roles_list}" -u ansible"
 echo ""
 
